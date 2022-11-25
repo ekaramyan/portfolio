841f24c164ba5c626d500b6e6d3bdd327961ad9d
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-26 02:46:36 +0400

    made header

---
+++ cv-app/src/App.jsx
@@ -0,0 +1,20 @@
+import './scss/styles.scss';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <header>
+        <a href='' className='logo'>Freelancer</a>
+        <nav className='nav'>
+          <ul className='nav__menu menu'>
+            <li className='menu__item'><a href=''>Projects</a></li>
+            <li className='menu__item'><a href=''>Skills</a></li>
+            <li className='menu__item'><a href=''>Contacts</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+// import './main';
+export default App;
