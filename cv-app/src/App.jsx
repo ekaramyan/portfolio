0d1388c27a9ee4df9bd4038c089422b95f6106f9
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-26 03:20:35 +0400

    made component instead of writing in one file

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,19 +1,14 @@
+import React, { Component } from 'react';
+
+
 import './scss/styles.scss';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='wrapper'>
-      <header>
-        <a href='' className='logo'>Freelancer</a>
-        <nav className='nav'>
-          <ul className='nav__menu menu'>
-            <li className='menu__item'><a href=''>Projects</a></li>
-            <li className='menu__item'><a href=''>Skills</a></li>
-            <li className='menu__item'><a href=''>Contacts</a></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <Header/>
+    // <div>pepe</div>
+    
   );
 }
 // import './main';
---
+++ cv-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class  Header extends React.Component {
+  render() {
+    return (
+        <div className='wrapper'>
+        <header>
+          <a href='' className='logo'>Freelancer</a>
+          <nav className='nav'>
+            <ul className='nav__menu menu'>
+              <li className='menu__item'><a href=''>Projects</a></li>
+              <li className='menu__item'><a href=''>Skills</a></li>
+              <li className='menu__item'><a href=''>Contacts</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
