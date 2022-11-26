39813f99ae870ec29b422db14834b352e0d17417
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-26 23:26:56 +0400

    remade components

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-
-
 import './scss/styles.scss';
 import Header from './components/Header';
-
+import Content1 from './components/Content1';
 
 function App() {
+
   return (
-    <Header/>
-    // <Content1/>
-    // <div>pepe</div>
-    
+    <div className="content">
+      <Header />
+      <Content1 />
+    </div>
   );
 }
+
 // import './main';
 export default App;
--- cv-app/src/components/Header.jsx
+++ cv-app/src/components/Header.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-import Content1 from './Content1';
+
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div className='content'>
-        <div className='wrapper'>
-          <header>
-            <a href='' className='logo'>Freelancer</a>
-            <nav className='nav'>
-              <ul className='nav__menu menu'>
-                <li className='menu__item'><a href=''>Projects</a></li>
-                <li className='menu__item'><a href=''>Skills</a></li>
-                <li className='menu__item'><a href=''>Contacts</a></li>
-              </ul>
-            </nav>
-          </header>
-        </div>
-        <Content1 />
-      </div>
+      <section className='header-nav'>
+        <header>
+          <a href='' className='logo'>Freelancer</a>
+          <nav className='nav'>
+            <ul className='nav__menu menu'>
+              <li className='menu__item'><a href=''>Projects</a></li>
+              <li className='menu__item'><a href=''>Skills</a></li>
+              <li className='menu__item'><a href=''>Contacts</a></li>
+            </ul>
+          </nav>
+        </header>
+      </section>
+
     )
   }
 }
